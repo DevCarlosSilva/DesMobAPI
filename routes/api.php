a38dbe8fba2455823de2c2cce48e82aeef62efd1
PedroJRodS <pedrojorgersilva@gmail.com>
2025-06-13 11:43:40 -0300

    feat: AuthController criado com rotas em api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('items', ItemController::class);
 Route::apiResource('reports', ReportController::class);
@@ -11,5 +12,17 @@ Route::apiResource('reports', ReportController::class);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(
+    function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    }
+);
 
 require __DIR__ . '/auth.php';
