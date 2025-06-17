50ed56ed28ea62c1b85a41a71d214e0f04f94000
PedroJRodS <pedrojorgersilva@gmail.com>
2025-06-17 11:46:15 -0300

    feat: Rota de Items e Reports no auth sanctum, só possível acessar se estiver logado e autenticado com sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,18 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\AuthController;
 
-Route::apiResource('items', ItemController::class);
-Route::apiResource('reports', ReportController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(
     function () {
+        Route::apiResource('items', ItemController::class);
+        Route::apiResource('reports', ReportController::class);
+
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', function (Request $request) {
             return $request->user();
