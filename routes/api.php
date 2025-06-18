eb17e88044da5b37bb947c0c081327f5ac8c65d0
PedroJRodS <pedrojorgersilva@gmail.com>
2025-06-18 11:46:11 -0300

    fixed: Rotas api items e relatos voltados para o middleware, autenticação funcionando

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,14 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('items', ItemController::class);
-Route::apiResource('reports', ReportController::class);
-
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:sanctum')->group(
     function () {
+        Route::apiResource('items', ItemController::class);
+        Route::apiResource('reports', ReportController::class);
 
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', function (Request $request) {
